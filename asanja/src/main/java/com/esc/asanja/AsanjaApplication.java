348eb3b2c7edf1fe26a675c75b0c9cb56f0599ca
Volodymyr Ovcharenko <volodymyyrr@gmail.com>
2019-05-19 01:31:11 +0300

    created simple project

---
+++ asanja/src/main/java/com/esc/asanja/AsanjaApplication.java
@@ -0,0 +1,13 @@
+package com.esc.asanja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsanjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsanjaApplication.class, args);
+	}
+
+}
---
+++ asanja/src/test/java/com/esc/asanja/AsanjaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.esc.asanja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsanjaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
